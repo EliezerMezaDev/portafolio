387a0262f8ed5a33734f46afbc541efefec54cee
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2023-11-15 15:55:32 -0400

    correcion en nombre de carpeta

---
+++ docs/assets/data.ts
@@ -0,0 +1,56 @@
+export const qualifications = [
+  {
+    timelapse: '2021 - Presente',
+    url: 'https://www.grupospartan.com/',
+    label: 'Grupo Spartan S.A.',
+
+    rol: 'Desarrollador de Software',
+    rol1: 'Analista de sistemas',
+    rol2: '',
+
+    description:
+      'impedit molestias reprehenderit voluptate pariatur sint asperiores. Error iusto accusantium excepturi sapiente unde eos earum ullam! Cum,',
+    skills: ['Ionic', 'Angular', 'React', 'TypeScript', 'JavaScript'],
+  },
+
+  {
+    timelapse: '2019 - 2021',
+    url: '#',
+    label: 'BeeLetters C.A.',
+
+    rol: 'Desarrollador Frontent',
+    rol1: '',
+    rol2: '',
+
+    description:
+      'impedit molestias reprehenderit voluptate pariatur sint asperiores. Error iusto accusantium excepturi sapiente unde eos earum ullam! Cum,',
+    skills: ['Wordpress', 'CSS'],
+  },
+];
+
+export const projects = [
+  {
+    label: 'Weather App',
+    description:
+      'Práctica de conexión con la REST API de api.openweathermap.org',
+    image: 'assets/previews/weaterappv01.png',
+    requireds: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://eliezermezadev.github.io/WeatherApp/',
+  },
+  {
+    label: 'Voice Converter App',
+    description:
+      'Práctica de uso de herramienta de síntesis de voz nativa de JavaScript',
+    image: 'assets/previews/voiceconverterappv01.png',
+    requireds: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://eliezermezadev.github.io/VoiceConverterApp/',
+  },
+  {
+    label: 'To-Do List App',
+    description:
+      'Práctica de captación e inserción de objetos en el DOM con JavaScript',
+    image: 'assets/previews/todolistappv01.png',
+    requireds: ['HTML', 'CSS', 'JavaScript'],
+    url: 'https://eliezermezadev.github.io/To-DoListApp_v01/',
+  },
+];
